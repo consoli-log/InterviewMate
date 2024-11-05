4691826227ce5052bd196a485ac8a7c3c14ec517
consoli.log <eumsoli@gmail.com>
2024-11-05 17:46:35 +0900

    🎉 Begin InterviewMate Project!

---
+++ src/test/java/com/iam/interviewmate/InterviewMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.interviewmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
